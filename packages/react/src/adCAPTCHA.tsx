c3fe31ac201d1b5f88f1ed6a33225ab5bd82f6d3
Lucasarenaterry <lucasat2000@hotmail.com>
2024-02-20 13:04:55 +0000

    Project setup to build correctly

--- packages/react/src/adCAPTCHA.tsx
+++ packages/react/src/adCAPTCHA.tsx
@@ -1,24 +1,28 @@
-import React, { useEffect } from 'react';
+// import React, { useEffect } from 'react';
 
-interface CaptchaProps {
-  onComplete: () => void;
+// interface CaptchaProps {
+//   onComplete: () => void;
+// }
+
+// export const adCAPTCHA: React.FC<CaptchaProps> = ({ onComplete }) => {
+//   useEffect(() => {
+//     const script = document.createElement('script');
+//     script.src = "https://widget.adcaptcha.com/index.js";
+//     script.defer = true;
+//     document.body.appendChild(script);
+
+//     window.adcap.setupTriggers({
+//       onComplete: () => {
+//         onComplete();
+//       }
+//     });
+//   }, [onComplete]);
+
+//   return (
+//     <div data-adcaptcha="PLACEMENT_ID" />
+//   );
+// };
+
+export const adCAPTCHA = () => {
+  return null;
 }
-
-export const adCAPTCHA: React.FC<CaptchaProps> = ({ onComplete }) => {
-  useEffect(() => {
-    const script = document.createElement('script');
-    script.src = "https://widget.adcaptcha.com/index.js";
-    script.defer = true;
-    document.body.appendChild(script);
-
-    window.adcap.setupTriggers({
-      onComplete: () => {
-        onComplete();
-      }
-    });
-  }, [onComplete]);
-
-  return (
-    <div data-adcaptcha="PLACEMENT_ID" />
-  );
-};
--- packages/react/tsup.config.ts
+++ packages/react/tsup.config.ts
@@ -2,14 +2,11 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig({
   entry: {
-    index: 'src/index.ts'
+    index: './src/index.ts'
   },
   external: ['react'],
   format: ['esm', 'cjs'],
   dts: true,
-  esbuildOptions(options) {
-    options.banner = {
-      js: '"use client";'
-    };
-  }
+  clean: true,
+  skipNodeModulesBundle: true
 });
