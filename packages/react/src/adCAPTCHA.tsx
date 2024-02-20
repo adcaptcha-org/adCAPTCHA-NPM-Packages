0c021e95737dce0035fe389ac111fc6f293ccf37
Lucasarenaterry <lucasat2000@hotmail.com>
2024-02-20 11:53:36 +0000

    Added tsup for quicker builds

---
+++ packages/react/src/adCAPTCHA.tsx
@@ -0,0 +1,24 @@
+import React, { useEffect } from 'react';
+
+interface CaptchaProps {
+  onComplete: () => void;
+}
+
+export const adCAPTCHA: React.FC<CaptchaProps> = ({ onComplete }) => {
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = "https://widget.adcaptcha.com/index.js";
+    script.defer = true;
+    document.body.appendChild(script);
+
+    window.adcap.setupTriggers({
+      onComplete: () => {
+        onComplete();
+      }
+    });
+  }, [onComplete]);
+
+  return (
+    <div data-adcaptcha="PLACEMENT_ID" />
+  );
+};
---
+++ packages/react/src/index.tsx
@@ -0,0 +1,1 @@
+export { adCAPTCHA } from './adCAPTCHA';
