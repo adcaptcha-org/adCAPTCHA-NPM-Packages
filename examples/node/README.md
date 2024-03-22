07dd1ce0b67a349beaa8f090067494b41b2206f7
Lucasarenaterry <lucasat2000@hotmail.com>
2024-03-22 12:32:13 +0000

    Added node package example

---
+++ examples/node/README.md
@@ -0,0 +1,9 @@
+# React | Typescript | TailwindCSS
+
+## Available Scripts
+
+In the project directory, you can run:
+
+### `node api.js`
+
+
--- examples/react/src/App.tsx
+++ examples/react/src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import './App.css';
 import { AdCAPTCHA, getSuccessToken } from '@adcaptcha/react';
+import axios from 'axios';
 import Navbar from './components/navbar';
 
 function App() {
@@ -10,6 +11,17 @@ function App() {
     const successToken = getSuccessToken();
     setToken(successToken);
   };
+
+  async function handleVerifyToken() {
+    try {
+      const response = await axios.post('http://localhost:3001/verifyToken', {
+        token: token,
+      });
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
   return (
     <div>
@@ -25,6 +37,7 @@ function App() {
               type="button"
               disabled={!token}
               className="rounded-md ml-10 bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 disabled:opacity-50 disabled:cursor-not-allowed"
+              onClick={handleVerifyToken}
             >
               Verify Captcha
             </button>
