2c83e76e73e117f075111958b031ca8c5d9ab2b2
Istvan Vas <swannxxx@yahoo.co.uk>
2024-11-14 10:23:46 +0000

    working on the svelte package

---
+++ packages/svelte/src/index.tsx
@@ -0,0 +1,4 @@
+import AdCAPTCHA from './components/adCAPTCHA.svelte';
+import { setKeywords, getSuccessToken } from './util'; 
+export { AdCAPTCHA, setKeywords, getSuccessToken };
+export default AdCAPTCHA;
